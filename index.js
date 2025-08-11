7aa4c8e4685080076f9b36612a95b09e23c007d2
devlrn <bcclrndev55@outlook.com>
2025-08-11 08:52:50 +0530

    modified all the files

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('Hello World Function triggered');
-    context.res = {
-        status: 200,
-        body: "Hello, World from Azure Function!"
-    };
-};
